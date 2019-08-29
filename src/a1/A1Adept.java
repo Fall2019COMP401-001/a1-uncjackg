ebcbe26556cb65be5579713fbc8cc00c96808ce9
uncjackg <jackg@live.unc.edu>
2019-08-29 15:57:11 -0400

    A1 Adept solution

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -8,7 +8,61 @@ public class A1Adept {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int itemCount = scan.nextInt();
 		
+		String[] itemList = new String[itemCount];
+		double[] priceList = new double[itemCount];
+		
+		for (int i=0; i<itemCount; i++) {
+			itemList[i] = scan.next();
+			priceList[i] = scan.nextDouble();
+		}
+		
+		int numCustomers = scan.nextInt();
+		
+		String[] customers = new String[numCustomers];
+		double[] moneySpent = new double[numCustomers];
+		
+		double sum = 0.0;
+		int biggestIndex = 0;
+		int smallestIndex = 0;
+		
+		for (int i=0; i<numCustomers; i++) {
+			customers[i] = scan.next() + " " + scan.next();
+			
+			int numItemsBought = scan.nextInt();
+			double totalSpent = 0.0;
+			
+			for (int j=0; j<numItemsBought; j++) {
+				int quantity = scan.nextInt();
+				String itemName = scan.next();
+				
+				for (int k=0; k<itemCount; k++) {
+					if (itemName.equals(itemList[k])) {
+						totalSpent += priceList[k] * quantity;
+					}
+				}
+			}
+			
+			moneySpent[i] = totalSpent;
+		
+			sum += totalSpent;
+
+			if (totalSpent > moneySpent[biggestIndex]) {
+				biggestIndex = i;
+			} else if (totalSpent < moneySpent[smallestIndex]) {
+				smallestIndex = i;
+			}
+		}
+		
+		scan.close();
+		
+		System.out.println("Biggest: " + customers[biggestIndex] + 
+				" (" + String.format("%.2f", moneySpent[biggestIndex]) + ")");
+		
+		System.out.println("Smallest: " + customers[smallestIndex] + 
+				" (" + String.format("%.2f", moneySpent[smallestIndex]) + ")");
+		
+		System.out.println("Average: " + String.format("%.2f", sum / numCustomers));
 	}
 }
