719a705b4ba87600d92d097c0ae63a02264e8220
uncjackg <jackg@live.unc.edu>
2019-08-29 10:38:19 -0400

    A1 novice solution

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,33 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
 		
+		int numCustomers = scan.nextInt();
+		String[] customers = new String[numCustomers];
+
+		for (int i=0; i<numCustomers; i++) {
+			char first = scan.next().charAt(0);
+			String lastName = scan.next();
+			
+			int count = scan.nextInt();
+			
+			double total = 0.00;
+			
+			for (int j=0; j<count; j++) {
+				int numItems = scan.nextInt();
+				scan.next();
+				double price = scan.nextDouble();
+				
+				total += price * numItems;
+			}
+			
+			customers[i] = first + ". " + lastName + ": " + 
+					String.format("%.2f", total);
+		}
+		
+		for (int i=0; i<numCustomers; i++) {
+			System.out.println(customers[i]);
+		}
+		scan.close();
 	}
 }
