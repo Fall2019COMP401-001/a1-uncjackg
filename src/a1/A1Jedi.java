b4b45d45dc8dd3b2a54b68242ad017d9ff35b341
uncjackg <jackg@live.unc.edu>
2019-08-29 21:43:45 -0400

    A1Jedi solution

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -8,7 +8,53 @@ public class A1Jedi {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int itemCount = scan.nextInt();
 		
+		String[] itemList = new String[itemCount];
+		
+		for (int i=0; i<itemCount; i++) {
+			itemList[i] = scan.next();
+			scan.nextDouble();
+		}
+		
+		int numCustomers = scan.nextInt();
+		
+		int[] count = new int[itemCount];
+		int[] customersBought = new int[itemCount];
+		
+		for (int i=0; i<numCustomers; i++) {
+			boolean[] alreadyBought = new boolean[itemCount];
+
+			scan.next();
+			scan.next();
+						
+			int itemsBought = scan.nextInt();
+			
+			for (int j=0; j<itemsBought; j++) {
+				int quantity = scan.nextInt();
+				String item = scan.next();
+				
+				for (int k=0; k<itemCount; k++) {
+					if (item.equals(itemList[k])) {
+						count[k] += quantity;
+						
+						if (!alreadyBought[k]) {
+							alreadyBought[k] = true;
+							customersBought[k]++;
+						}
+					}
+				}
+			}
+		}
+		
+		for (int i=0; i<itemCount; i++) {
+			if (customersBought[i] == 0) {
+				System.out.println("No customers bought " + itemList[i]);
+			} else {
+				System.out.println(customersBought[i] + " customers bought " + count[i] + " " + itemList[i]);
+			}			
+		}
+		
+		scan.close();
 	}
 }
